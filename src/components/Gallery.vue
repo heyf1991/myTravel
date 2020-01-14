8ce1c363accc24b22054799f393fde4d8f290426
heyf <heyf@ourchem.com>
2020-01-14 11:21:45 +0800

    gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,20 +1,68 @@
 <template>
   <div class="gallery" @click.stop="toCloseGallery">
-    <div>
-      <img src="https://dimg04.c-ctrip.com/images/300n0o000000fbhfb8E71_C_750_420_Q90.jpg" />
-    </div>
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide
+        ><img class="swiper-img" src="https://dimg04.c-ctrip.com/images/300m0q000000geb5o1606_C_750_420_Q90.jpg" alt=""
+      /></swiper-slide>
+      <swiper-slide
+        ><img class="swiper-img" src="https://dimg04.c-ctrip.com/images/300n0o000000fbhfb8E71_C_750_420_Q90.jpg" alt=""
+      /></swiper-slide>
+      <swiper-slide
+        ><img class="swiper-img" src="https://dimg04.c-ctrip.com/images/300c0q000000g7ew0F6C1_C_750_420_Q90.jpg" alt=""
+      /></swiper-slide>
+      <swiper-slide
+        ><img class="swiper-img" src="https://dimg04.c-ctrip.com/images/300g0q000000gm7l14774_C_750_420_Q90.png" alt=""
+      /></swiper-slide>
+      <!-- Optional controls -->
+    </swiper>
+    <div class="swiper-pagination" slot="pagination"></div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          type: 'fraction',
+        },
+        loop: false,
+        observer: true,
+        observeParents: true,
+      },
+    }
   },
-  components: {},
-  created() {},
-  computed: {},
+  mounted() {
+    /* console.log('啦啦啦啦', this.swiper)
+
+    this.swiper.slideTo(0, 2000, false) */
+    let t = setTimeout(() => {
+      this.swiperOption = {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          type: 'fraction',
+        },
+        loop: false,
+        observer: true,
+        observeParents: true,
+      }
+      this.swiper.slideTo(0, 2000, false)
+      this.swiper.init()
+      clearTimeout(t)
+    }, 1000)
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper
+    },
+  },
   methods: {
-    toCloseGallery() {
+    toCloseGallery(e) {
+      if (e.target.className === 'swiper-img') return
       this.$emit('handleCloseGallery')
     },
   },
